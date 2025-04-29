ba0beb23276e60466f2a99289e372ae5b7dde7ca
bartolomeusantos072 <bartolomeusantos072@gmail.com>
2025-04-29 03:20:08 -0300

    finalizar

--- javascript/products.js
+++ javascript/products.js
@@ -4,48 +4,60 @@ export default [
     name: 'Dogão Tradicional', 
     price: 10,
     description: 'Pão, salsicha, batata palha, milho, presunto, ketchup e maionese',
-    img: 'img/tradicional.jpg'
+    img: 'img/hotdog.png'
   },
   { 
     id: 'completo', 
     name: 'Dogão Completo', 
     price: 12,
     description: 'Pão, salsicha, milho, batata palha, presunto, ovo de codorna, uva passa, vinagrete, queijo ralado, ketchup e maionese',
-    img: 'img/cheddar.jpg'
+    img: 'img/hotdog.png'
   },
   { 
     id: 'Duplo', 
     name: 'Dogão Duplo', 
     price: 13,
     description: 'Pão, 2 salsichas, milho, batata palha, presunto, ovo de codorna, uva passa, vinagrete, queijo ralado, ketchup e maionese',
-    img: 'img/bacon.jpg'
+    img: 'img/hotdog.png'
   },
   { 
     id: 'duplo', 
     name: 'Dogão Calabresa', 
     price: 15,
     description: 'Pão, calabresa, milho, batata palha, presunto, ovo de codorna, uva passa, vinagrete, queijo ralado, ketchup e maionese',
-    img: 'img/duplo.jpg'
+    img: 'img/hotdog.png'
   },
   { 
     id: 'frango', 
     name: 'Dogão Frango', 
     price: 16,
     description: 'Pão, frango, milho, batata palha, presunto, ovo de codorna, uva passa, vinagrete, queijo ralado, ketchup e maionese',
-    img: 'img/especial.jpg'
+    img: 'img/hotdog.png'
   },
   { 
     id: 'refri', 
     name: 'Refrigerante Lata', 
     price: 5,
     description: 'Escolha entre Coca-Cola, Guaraná, Fanta ou Sprite',
-    img: 'img/refri.jpg'
+    img: 'img/lata.jpg'
   },
   { 
-    id: 'agua', 
+    id: 'refri', 
     name: 'Agua', 
     price: 5,
     description: 'Escolha entre Coca-Cola, Guaraná, Fanta ou Sprite',
-    img: 'img/refri.jpg'
+    img: 'img/lata.jpg'
+  }, { 
+    id: 'refri', 
+    name: 'Agua', 
+    price: 5,
+    description: 'Escolha entre Coca-Cola, Guaraná, Fanta ou Sprite',
+    img: 'img/lata.jpg'
+  }, { 
+    id: 'refri', 
+    name: 'Agua', 
+    price: 5,
+    description: 'Escolha entre Coca-Cola, Guaraná, Fanta ou Sprite',
+    img: 'img/lata.jpg'
   }
 ];
